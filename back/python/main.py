b2b8b82a71d7b2927d6da33ef5b455bfa1ad8e5c
Andrew97422 <nosoff.4ndr@yandex.ru>
2023-11-30 13:37:02 +0300

    Добавлена работа с кандинским

--- back/python/main.py
+++ back/python/main.py
@@ -1,1 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, send_file
+import requests
+from text_to_image import *
+import json
+
+app = Flask(__name__)
+
+api = Text2ImageAPI('https://api-key.fusionbrain.ai/',
+                    '0E8304C916C8A0D2A63B6D58820DB253',
+                    'A4CA30389EC1BE639594E6C144523A14'
+                    )
+
+
+@app.route('/api/v1/models')
+def get_model():
+    return str(api.get_model())
+
+
+@app.route('/api/v1/generate', methods=['GET', 'POST'])
+def generate():
+    result = json.loads(request.data)
+    id_model = api.get_model()
+    return str(api.generate(result['params']['generateParams']['query'], id_model, result['params']['numImages'],
+                            result['params']['width'], result['params']['height']))
+
+
+@app.route('/api/v1/check_generation')
+def check_generation():
+    return str(api.check_generation(json.loads(request.data)['uuid']))
+
+
+app.run()
